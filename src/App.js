eb84aaf635baaee2ca2927bd3aa5ea578a7616c0
amu8051 <ramit9115@gmail.com>
2022-09-29 10:44:22 +0530

    Core Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import ImageView from "./components/ImageView";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  const API_KEY='30205654-82c446a876fb65adfa2f526e7';
+
+// const Term = 'yellow flowers';
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&pretty=true`)
+    .then((response) => { return response.json();})
+    .then((data) => {
+      setImages(data.hits); setIsLoading(false);
+    })
+    .catch((error) => {console.log(error, 'To err is human!!')});
+  }, [query]);
+
+  if(isLoading){
+    return(
+      <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <ImageSearch searchTerm={(query) => { setQuery(query)}}/>
+      {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">Oops.No Image Found!</h1>}
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((img) => {  
+        return <ImageView key={img.id} image={img}/>
+        })}
+      </div>
     </div>
   );
 }
